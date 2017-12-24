06110865142fe594a68a150a8ca86bb64cfcc327
Yegor Kozlov <e.kozlov@softteco.com>
2017-12-24 13:19:14 +0300

    Add second part of day4 task

--- day4/solution.js
+++ day4/solution.js
@@ -5,7 +5,20 @@ function isPhraseValid(phrase) {
     return words.every((word) => words.lastIndexOf(word) === words.indexOf(word));
 }
 
+function sortLettersInPhraseWords(phrase) {
+    const words = phrase.split(' ');
+    return words
+        .map((word) => word.split('').sort().join(''))
+        .join(' ');
+}
+
 const testData = fs.readFileSync('data.txt', 'utf8');
 const phrases = testData.split('\n');
+
 const correctPassPhrases = phrases.filter(isPhraseValid);
-console.log('answer', correctPassPhrases.length);
+console.log('ResultA: ', correctPassPhrases.length);
+
+const correctPhrasesWithSortedLetters = phrases
+    .map(sortLettersInPhraseWords)
+    .filter(isPhraseValid);
+console.log('ResultB: ', correctPhrasesWithSortedLetters.length);
